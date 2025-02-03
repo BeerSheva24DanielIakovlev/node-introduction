b2bff3eb808809df23843a6368257d11a95f1b1e
Daniel <gromdan90@gmail.com>
2025-02-03 11:00:38 +0200

    async

--- index.mjs
+++ index.mjs
@@ -1,7 +1,16 @@
 import logger from './logger.mjs';
 import fs from 'node:fs';
 
-const data = fs.readFileSync('./index.mjs', 'utf8');
-logger.info(data);
+const data = fs.readFileSync('./file.txt', 'base64', (err, data) => {
+    if (err) {
+        logger.error(err);
+    } else {
+        console.log(data);
+        
+    }
+});
+console.log(data);
 
-fs.writeFileSync('./file.txt', ["kuku", "kukareku","hjgasjhsa", "Hello World!"].join('\n'), 'utf8');
+
+fs.writeFileSync('./file.txt', ["kuku", "kukareku","hjgasjhsa", "Hello World!"].join('\n'),
+  'utf-16le', () => logger.debug('file saved in utf-16le format'));
