617f9c5f90b4d45b23a94bf96ba4072ea54020f3
Daniel <gromdan90@gmail.com>
2025-02-03 10:34:37 +0200

    sync

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import logger from './logger.mjs';
 import fs from 'node:fs';
 
-const data = fs.readFileSync('./index.mjs');
+const data = fs.readFileSync('./index.mjs', 'utf8');
 logger.info(data);
 
-fs.writeFileSync('./file.txt', ["kuku", "kukareku","hjgasjhsa", "Hello World!"].join('\n'));
+fs.writeFileSync('./file.txt', ["kuku", "kukareku","hjgasjhsa", "Hello World!"].join('\n'), 'utf8');
